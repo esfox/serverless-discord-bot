307925ae90ff1e2d061e8ea76fad7ecc8aa335b0
esfox <osairis100@gmail.com>
2022-01-08 22:49:36 +0800

    Use Discord.js builder for slash command

--- register-commands.js
+++ register-commands.js
@@ -1,21 +1,22 @@
 import got from 'got';
+import { SlashCommandBuilder } from '@discordjs/builders';
 
-const hello =
-{
-  name: 'hello',
-  description: 'hello command',
-};
+const hello = new SlashCommandBuilder()
+  .setName('hello')
+  .setDescription('hello command')
+  .toJSON();
 
 const discordCommandsUrl = `https://discord.com/api/v8/applications/${process.env.APPLICATION_ID}/commands`;
 
+const apiClient = got.extend({
+  headers: { Authorization: `Bot ${process.env.TOKEN}` },
+});
+
 (async () =>
 {
-  const response = await got.put(discordCommandsUrl, {
-    headers: { Authorization: `Bot ${process.env.TOKEN}` },
+  const response = await apiClient.put(discordCommandsUrl, {
     json: [hello],
   });
-
-  console.log(response.body);
 
   if(response.statusCode === 200)
     console.log('Successfully registered commands');
